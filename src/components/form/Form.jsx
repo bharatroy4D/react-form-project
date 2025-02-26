d50f1752e7a3cc5d6d3cdeb6f2b991f90097a952
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-02-26 12:26:13 +0600

    form into onchange method added

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import { useState } from "react";
 
 const Form = () => {
+    const [name, setName] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [number, setNumber] = useState(null);
+
     const handleSubmit = e => {
         e.preventDefault();
-        console.log(e.target.name.value)
-        console.log(e.target.email.value)
-        console.log(e.target.number.value)
-
+      console.log(name , email, number)
     }
+
+    const handleNameChanges = e =>{
+        setName(e.target.value);
+    }
+    const handleEmailChanges = e =>{
+        setEmail(e.target.value);
+    }
+    const handleNumberChanges = e =>{
+        setNumber(e.target.value);
+    }
+
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" name="name" id="" /><br /><br />
-                <input type="text" name="email" id="" /><br /><br />
-                <input type="number" name="number" id="" /><br /><br />
+                <input onChange={handleNameChanges} type="text" name="name" id="" /><br /><br />
+                <input onChange={handleEmailChanges} type="text" name="email" id="" /><br /><br />
+                <input onChange={handleNumberChanges} type="number" name="number" id="" /><br /><br />
                 <input type="submit" name="submit" id="" />
             </form>
         </div>
