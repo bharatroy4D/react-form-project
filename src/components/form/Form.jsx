d2d009b2c98a8d30553f1c322d66795bef8338a1
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-02-26 11:29:27 +0600

    form component added

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
 const Form = () => {
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(e.target.name.value)
+        console.log(e.target.email.value)
+        console.log(e.target.number.value)
+
+    }
     return (
         <div>
-            <form>
-                <input type="text" name="name" id="" /><br /><br />                               
-                <input type="text" name="email" id="" /><br /><br />                               
-                <input type="number" name="number" id="" /><br /><br />  
-                <input type="submit" name="submit" id="" />                             
-
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" id="" /><br /><br />
+                <input type="text" name="email" id="" /><br /><br />
+                <input type="number" name="number" id="" /><br /><br />
+                <input type="submit" name="submit" id="" />
             </form>
         </div>
     );
